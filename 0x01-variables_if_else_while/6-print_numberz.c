52bcad8655e3e767d1062dd6f1d1b43221518666
Sameh ElGazar <selgazar95@gmail.com>
2023-08-11 16:48:05 -0700

    Task 7 Push 5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * descrition: Print from 0..1 digit just using putchar
+ *
+ * Return: Always (0) Success
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar('i' + 0);
+	}
+	return (0);
+}

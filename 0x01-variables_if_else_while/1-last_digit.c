3d16517087de05db5fc5869b9a87609954b2a821
Sameh ElGazar <selgazar95@gmail.com>
2023-08-11 13:39:42 -0700

    Second Task Push 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
 	else if (n % 10 == 0)
-	{		
+	{
 		printf("Last digit of %d is %d and is %d\n", n, n % 10, n % 10);
 	}
 	else if (n % 10 < 6 && n % 10 != 0)

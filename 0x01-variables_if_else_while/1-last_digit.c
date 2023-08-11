a018216db25f1bf1a61722b5bc06df2b830817cc
Sameh ElGazar <selgazar95@gmail.com>
2023-08-11 13:38:38 -0700

    Second Task Push 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,7 +7,7 @@
  *
  * Description: The last digit using modulus.
  *
- * Return: always (0) Success
+ * Return: always (0) Succes
 */
 int main(void)
 {
@@ -16,18 +16,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	
-	if(n%10 > 5)
+	if (n % 10 > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n%10);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
-	else if(n%10 == 0)
+	else if (n % 10 == 0)
 	{		
-		printf("Last digit of %d is %d and is %d\n", n, n%10, n%10);
+		printf("Last digit of %d is %d and is %d\n", n, n % 10, n % 10);
 	}
-	else if(n%10 < 6 && n%10 != 0)
+	else if (n % 10 < 6 && n % 10 != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n%10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 	return (0);
 }

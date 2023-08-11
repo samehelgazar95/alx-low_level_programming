6ed4cbf10064ac781318dc9e0837efbf22c58ec6
Sameh ElGazar <selgazar95@gmail.com>
2023-08-11 12:37:07 -0700

    First Task Push5

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -23,11 +23,11 @@ int main(void)
 	}
 	else if (n == 0)
 	{
- 		 printf("%d is zero", n);
+ 		printf("%d is zero", n);
 	}
 	else if (n < 0)
 	{
-		 printf("%d is negative", n);
+		printf("%d is negative", n);
 	}
 	return (0);
 }

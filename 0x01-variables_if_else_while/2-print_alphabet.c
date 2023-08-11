c34f00eb1aa139ecaa99c459d10d9bfa60317b2a
Sameh ElGazar <selgazar95@gmail.com>
2023-08-11 13:48:26 -0700

    Third Task Push 0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: Print alphabt
+ *
+ * Return: Always 0 Success
+*/
+nt main(void)
+{
+	puchar("abcdefghijklmnopqrstuvwxyz")
+	return (0)
+}

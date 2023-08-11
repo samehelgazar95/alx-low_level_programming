965e8495025417ab352c14bd48d0ef5b65ac0d07
Sameh ElGazar <selgazar95@gmail.com>
2023-08-11 16:04:44 -0700

    Task 6 Push 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: print digit numbers 0..1
+ *
+ * Return: Always (0) Success
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 11)
+	{
+		printf("%d", i);
+		i++;
+	}
+	return (0);
+}

c6b3ff50691e49349edbd4f97d4cec195bf1750c
Sameh ElGazar <selgazar95@gmail.com>
2023-08-12 16:23:33 -0700

    Task 11 Push 5

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints all possible different combinations of three digits
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int num1 = 0, num2, num3;
+
+	while (num1 < 10)
+	{
+		num2 = num1 + 1;
+		while (num2 < 10)
+		{
+			while (num3 < 10)
+			{
+				num3 = num2 + 1;
+				putchar(num1 + 48);
+				putchar(num2 + 48);
+				putchar(num3 + 48);
+				if (num1 + num2 + num3 != 24)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				num3++;
+			}
+
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}

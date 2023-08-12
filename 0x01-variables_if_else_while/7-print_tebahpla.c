5290a3e75bf4a7b5a5c2987a715f2f918677002a
Sameh ElGazar <selgazar95@gmail.com>
2023-08-11 17:24:52 -0700

    Task 7 Push 0

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: print alphabet in rev
+ *
+ * Return: Always (0) Success
+*/
+
+int main(void)
+{
+	char c = 122;
+
+	while (c <= 97)
+	{
+		putchat(c);
+		c--;
+	}
+
+	putchar('\n');
+	return (0);
+}

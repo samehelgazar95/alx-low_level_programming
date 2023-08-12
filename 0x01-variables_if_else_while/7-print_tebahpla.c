be1adb6ceb7b1e450915b9a4158a4dc2018ec6bb
Sameh ElGazar <selgazar95@gmail.com>
2023-08-11 17:15:36 -0700

    Task 8 Push 0

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: print Hexa Numbers
+ *
+ * Return: Always (0) Success
+*/
+
+int main(void)
+{
+	int i = 48;
+	while (i < 102)
+	{
+		putchar(i);
+		if(i == 57)
+		{
+			i = 96;
+		}
+		
+		i++;
+	}
+	return (0)
+}

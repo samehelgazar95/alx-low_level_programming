d6513f39a7f1fadb294e1076128e9bd902073f61
Sameh ElGazar <selgazar95@gmail.com>
2023-08-13 08:52:00 -0700

    Task 13 Push 1

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: prints all possible combinations of two two-digit numbers
+ *
+ * return: 0 (Success)
+*/
+
+int main(void)
+{
+	int n1 = 0, n2;
+
+	while (n1 <= 99)
+	{
+		n2 = n1;
+		while (n2 <= 99)
+		{
+			if (n1 != n2)
+			{
+				putchar(n1 / 10 + 48);
+				putchar(n1 % 10 + 48);
+				putchar(' ');
+				putchar(n2 / 10 + 48);
+				putchar(n2 % 10 + 48);
+				if (n1 != 98 || n2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			n2++;
+		}
+		n1++;
+	}
+	return (0);
+}

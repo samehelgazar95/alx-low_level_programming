8bde1523ebf318a1a89a5323e1dc775367c1a38c
Sameh ElGazar <selgazar95@gmail.com>
2023-08-15 14:56:56 -0700

    Task 5 Push 1

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs -  computes the absolute value of an integer.
+ *
+ * @num: the param that user enters
+ *
+ * Return: integer (Success)
+*/
+
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (num * -1);
+	}
+
+	return (num)
+}

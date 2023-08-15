4c954c908da556df9edbd9afb723a85215dd8d3a
Sameh ElGazar <selgazar95@gmail.com>
2023-08-15 13:11:24 -0700

    Task 3 Push 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print alphabets 10 times
+*/
+
+void print_alphabet_x10(void)
+{
+	int line = 0;
+
+	while (line <= 10)
+	{
+		int c = 'a';
+
+		while (c <= 'z')
+		{
+			_puchar(c);
+			c++;
+		}
+		_putchar('\n');
+		line++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
+
 int _putchar(char c);
 
 void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
 #endif

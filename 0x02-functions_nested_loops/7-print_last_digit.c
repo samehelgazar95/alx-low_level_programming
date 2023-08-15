cc0e03c134254862477dda5fa07b1ee17d4b5840
Sameh ElGazar <selgazar95@gmail.com>
2023-08-15 16:08:20 -0700

    Task 7 Push 2

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * print_last_digit - prints the last digit of a number.
+ *
+ * @r: param to check for
+ *
+ * Return: @r % 10
+*/
+
 int print_last_digit(int r)
 {
 	r = _abs(r);

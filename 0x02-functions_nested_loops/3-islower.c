abec8f4bac7adb5ed60fd462e445fa6613daa3e6
Sameh ElGazar <selgazar95@gmail.com>
2023-08-15 13:33:35 -0700

    Task 4 Push 1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower -  Write a function that checks for lowercase character.
+ * @c: the char to check
+ *
+ * Return: 1 (Lower) - 0 (Otherwise)
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		putchar(1 + '0');
+	}
+	else if (c >= 'A' && '0')
+	{
+		putchar(0 + '0')
+	}
+}

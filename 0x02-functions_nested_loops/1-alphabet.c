7374c6bcbd859ecf4175d237e43e520ccc076fb9
Sameh ElGazar <selgazar95@gmail.com>
2023-08-15 12:55:41 -0700

    Task 2 Push 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet() util to print alphabets in lower case
+*/
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}

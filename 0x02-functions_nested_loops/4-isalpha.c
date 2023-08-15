9d70b95111327c4724a5a7cc808e85388621cbb8
Sameh ElGazar <selgazar95@gmail.com>
2023-08-15 15:11:27 -0700

    Task 4 Push 1

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha -  checks for alphabetic character.
+ *
+ * @c: param to check
+ *
+ * Return: 1 (true), 0 (false)
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 65 && a <= 90 || c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}

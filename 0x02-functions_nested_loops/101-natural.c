d402c0d2187c5c60a59b293a2dd6362a66b9b325
Sameh ElGazar <selgazar95@gmail.com>
2023-08-16 08:11:28 -0700

    task 14 Push 2

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,10 +10,14 @@ int main(void)
 	int three = 0, five = 0, sum, i;
 
 	for (i = 0; i < 1024; i += 3)
+	{
 		three += i;
+	}
 
 	for (i = 0; i < 1024; i += 5)
+	{
 		five += i;
+	}
 
 	sum = three + five;
 	printf("%d\n", sum);

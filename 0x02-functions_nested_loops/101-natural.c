5cedf353933508ed217a77820316918ed28a87b7
Sameh ElGazar <selgazar95@gmail.com>
2023-08-16 08:09:40 -0700

    task 14 Push 1

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ * Return: 0 (Success).
+*/
+
+int main(void)
+{
+	int three = 0, five = 0, sum, i;
+
+	for (i = 0; i < 1024; i += 3)
+		three += i;
+
+	for (i = 0; i < 1024; i += 5)
+		five += i;
+
+	sum = three + five;
+	printf("%d\n", sum);
+
+	return (0);
+}

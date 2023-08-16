173ee14d61e2220486dadb0a52250e5d044bd12e
Sameh ElGazar <selgazar95@gmail.com>
2023-08-16 06:45:10 -0700

    task 9 Push 8

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,15 +2,26 @@
 
 void times_table(void)
 {
-	int num = 0;
-
 	for (int n1 = 0; n1 < 10; n1++)
 	{
-		for (int n2 = 0; n2 < 10; n2++)
+		_putchar(48);
+		for (int n2 = 1; n2 <= 9; n2++)
 		{
-			num += n2;
-			_putchar('num' + '0');
+			_putchar(',');
+			_putchar(' ');
+
+			int num = n1 * n2;
+
+			if (num <= 9)
+			{
+				_putchar((num % 10) + '0');
+			}
+			else
+			{
+				_putchar((num / 10) + '0');
+				_putchar((num % 10) + '0');
+			}
 		}
-		num = 0;
+		_putchar('\n');
 	}
 }

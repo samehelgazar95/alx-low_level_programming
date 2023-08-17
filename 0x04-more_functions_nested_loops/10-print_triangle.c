002473a966dfb53fe03b6449e48f7ed5407552b8
Sameh ElGazar <selgazar95@gmail.com>
2023-08-17 12:43:59 -0700

    Task 11 Push 1

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line.
+ * @size: hieght of the triangle
+*/
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if ((i + j) > size)
+				{
+					_putchar('#');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+		}
+	}
+}

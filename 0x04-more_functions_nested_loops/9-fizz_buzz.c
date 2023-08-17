f946b4449973c3df7ba5b59e5b0d6f43145f406c
Sameh ElGazar <selgazar95@gmail.com>
2023-08-17 09:05:23 -0700

    Task 10 Push 8

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,22 +16,30 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 3 != 0 && i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
+		}
+
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+		else if (i == 100)
+		{
+			putchar('\n');
 		}
 	}
 
-	putchar('\n');
 	return (0);
 }

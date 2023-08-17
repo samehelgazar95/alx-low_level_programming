4d08ab338153e50bfe11ea9c9413bac34040a832
Sameh ElGazar <selgazar95@gmail.com>
2023-08-17 08:46:14 -0700

    Task 10 Push 1

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Fizz Buzz Program
+ *
+ * Return: 0 (Success)
+ * */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	return (0);
+}

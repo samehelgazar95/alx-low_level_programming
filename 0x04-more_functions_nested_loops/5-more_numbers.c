afd294a4e91d3018dd5ef216e399371a60fa804c
Sameh ElGazar <selgazar95@gmail.com>
2023-08-17 05:56:30 -0700

    Task 5\ Push 3

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,26 +3,23 @@
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
  *
- * Return: numbers from 1 - 14 ,Ten times
+ * Return: result
 */
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, num;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j < 10)
+			num = j;
+			if (j < 9)
 			{
-				_putchar(j + 48);
+				_putchar((num / 10) + 48);
 			}
-			else
-			{
-				_putchar((j / 10) + 48);
-				_putchar((j % 10) + 48);
-			}
+			_putchar((num % 10) + 48);
 		}
 		_putchar('\n');
 	}

a94eea7600708c8e5866727c901058d81c212435
Sameh ElGazar <selgazar95@gmail.com>
2023-08-17 05:44:14 -0700

    Task 5\ Push 2

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,17 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; i < 15; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j + 48);
+			if (j < 10)
+			{
+				_putchar(j + 48);
+			}
+			else
+			{
+				_putchar((j / 10) + 48);
+				_putchar((j % 10) + 48);
+			}
 		}
 		_putchar('\n');
 	}

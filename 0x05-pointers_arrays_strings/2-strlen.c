1fe08ae92b2ac09fd025a0cb9b3954c15f18bae5
Sameh ElGazar <selgazar95@gmail.com>
2023-08-21 15:53:11 -0700

    Task 3 Push 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: array to calc it's length
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+
+	return i;
+}

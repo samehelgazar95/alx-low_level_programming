84007f9f593a891e4f628800a5758dda3f995b93
Sameh ElGazar <selgazar95@gmail.com>
2023-08-21 17:13:11 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - Reverses a string
+ * @s: string to be reversed
+ *
+ * Return: Reversed string
+*/
+
+void rev_string(char *s)
+{
+	char temp;
+	int len = strlen(s) - 1, i = 0;
+	
+	while (len > i)
+	{
+		temp = s[len];
+		s[len] = s[i];
+		s[i] = s[len];
+	}
+}

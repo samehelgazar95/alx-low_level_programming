48c7d101da3e26e6c82d35d53b3b48299a0e2a21
Sameh ElGazar <selgazar95@gmail.com>
2023-08-23 18:42:18 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+/**
+ * string_toupper - lower to upper
+ * str: string to convert
+ *
+ * Return: string of the converted
+*/
+
+char *string_toupper(char *str)
+{
+	while (*str)
+	{
+		if (*str > 97 && *str < 122)
+		{
+			*str -= 32;
+		}
+		str++;
+	}
+
+	return (str);
+}

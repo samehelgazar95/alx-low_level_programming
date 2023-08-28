fb2745889f0e1e8bee7af72de2d327a16ce76e28
Sameh ElGazar <selgazar95@gmail.com>
2023-08-28 06:35:29 -0700

    0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,22 +3,6 @@
 
 int _putchar(char c);
 
-char *_strcat(char *dest, char *src);
-
-char *_strncat(char *dest, char *src, int n);
-
-char *_strncpy(char *dest, char *src, int n);
-
-int _strcmp(char *s1, char *s2);
-
-void reverse_array(int *a, int n);
-
-char *string_toupper(char *str);
-
-char *cap_string(char *);
-
-char *leet(char *)
-
-char *rot13(char *);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif

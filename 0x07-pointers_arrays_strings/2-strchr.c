90a51150b267116ecfe0fd3f4ed1e1cd4a23f04d
Sameh ElGazar <selgazar95@gmail.com>
2023-08-28 08:58:49 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+/**
+ * _strchr - locates a character in a string.
+ * @s: string to check in
+ * @c: char to check for
+ *
+ * Return: Pointer for the first occurrence in s for c
+ * or NULL if not found
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return (NULL);
+}

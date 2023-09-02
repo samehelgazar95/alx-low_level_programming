981fd02856acda508f00dc4271309d142553a2fd
Sameh ElGazar <selgazar95@gmail.com>
2023-09-02 05:17:59 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,14 +9,18 @@
 
 int main(int argc, char *argv[])
 {
+	int res;
+
 	if (argc == 3)
 	{
 		printf("%d\n", (*argv[1] - 48) * (*argv[2] - 48));
-		return (0);
+		res = 0;
 	}
 	else if (argc != 3)
 	{
 		printf("Error\n");
-		return (1);
+		res = 1;
 	}
+
+	return (res);
 }

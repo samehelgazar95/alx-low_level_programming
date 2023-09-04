9606d21ffab99d433784b1d0a0dcd76f26ca279c
Sameh ElGazar <selgazar95@gmail.com>
2023-09-04 14:42:35 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup- duplicate string
+ * @str: str to copy
+ * Return: str or null
+*/
+
+char *_strdup(char *str)
+{
+	char *dup;
+	int size;
+
+	for (size = 0; str[size] != '\0'; size++)
+	{}
+
+	dup = (char *)malloc(sizeof(char) * size);
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+
+	for (size = 0; str[size] != '\0'; size++)
+	{
+		dup[size] = str[size];
+	}
+
+	return (dup);
+}

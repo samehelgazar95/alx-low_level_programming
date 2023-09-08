e4e3205597d17699193382c78d15b13de8794fe1
Sameh ElGazar <selgazar95@gmail.com>
2023-09-07 17:50:26 -0700

    0

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,9 +42,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		concatedString[i] = s1[i];
 	}
 
-	for (j = 0; j < n; j++, i++)
+	for (j = 0; j < n; j++)
 	{
 		concatedString[i] = s2[j];
+		i++;
 	}
 
 	concatedString[i] = '\0';

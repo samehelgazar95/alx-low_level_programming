c2cc281b9a94877495bd9dd8d1f723c0e63fdfd7
Sameh ElGazar <selgazar95@gmail.com>
2023-09-07 18:14:04 -0700

    0

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int firstLen, secondLen, concatedLen, i, j;
-	char *concatedString = NULL;
+	char *concatedString;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -20,24 +20,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (firstLen = 0; *s1 != '\0'; firstLen++)
-	{}
+	for (firstLen = 0; s1[firstLen] != '\0'; firstLen++)
+		;
 
-	for (secondLen = 0; *s2 != '\0'; secondLen++)
-	{}
+	for (secondLen = 0; s2[secondLen] != '\0'; secondLen++)
+		;
 
 	if (n > secondLen)
 		n = secondLen;
 
 	concatedLen = firstLen + n;
 
-	concatedString = (char *)malloc(concatedLen + 1);
+
+	concatedString = malloc(concatedLen + 1);
 	if (concatedString == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; *s1 != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		concatedString[i] = s1[i];
 	}

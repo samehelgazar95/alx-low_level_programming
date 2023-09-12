7e2614081ceaf04548db84a73be5532720352f2d
Sameh ElGazar <selgazar95@gmail.com>
2023-09-12 08:16:06 -0700

    Structures, typedef 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog- dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+*/
+
 struct dog
 {
 	char *name;

36b046b74517a27127f23a9bb386af6d86d99b2d
Sameh ElGazar <selgazar95@gmail.com>
2023-09-12 14:30:13 -0700

    0x0E. C - Structures, typedef 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,17 +4,19 @@
 /**
  * free_dog- free dogs and run away
  * @d: the dog or the dogs
- * Return: Nothing, it just frees dog or dogs
+ * Return: Nothing
 */
 
 void free_dog(dog_t *d)
 {
-	if (d->name)
-		free(d->name);
-
-	if (d->owner)
-		free(d->owner);
-
 	if (d)
+	{
+		if (d->name)
+			free(d->name);
+
+		if (d->owner)
+			free(d->owner);
+
 		free(d);
+	}
 }

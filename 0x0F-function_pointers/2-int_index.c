fe7b3991001c0fc42204038c9b678c884e4665c9
Sameh ElGazar <selgazar95@gmail.com>
2023-09-13 12:38:35 -0700

    0x0F. C - Function pointers - 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
 
 /**
- * int_index-
- * @array:
- * @size:
- * @cmp:
- * Return:
+ * int_index- searches for an integer.
+ * @array: the array holding numbers
+ * @size: the size of the arr
+ * @cmp: the compate function
+ * Return: index or -1 or 0
 */
 
 int int_index(int *array, int size, int (*cmp)(int))

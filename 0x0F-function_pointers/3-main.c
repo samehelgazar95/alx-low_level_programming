0938c0198c133b27d231463ebe7c5b01cbf141a0
Sameh ElGazar <selgazar95@gmail.com>
2023-09-13 17:51:54 -0700

    0x0F. C - Function pointers - 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	if (!num2 && (argv[2][0] == '/' || argv[2][0] == '%'))
+	if (num2 == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
 		exit(100);

9e26158f63a4f99f2bb123fb5f672f966f2b0af3
Sameh ElGazar <selgazar95@gmail.com>
2023-09-14 11:57:43 -0700

    0x10. C - Variadic functions  - 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum 'em all
+ * @n: size of nums to sum
+ * Return: Sum of all
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list list;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		int num = va_arg(list, int);
+
+		sum += num;
+	}
+
+	va_end(list);
+
+	return (sum);
+}

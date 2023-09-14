d13b2d94b5ccebfee13506c7b7e6160d04bfa4c9
Sameh ElGazar <selgazar95@gmail.com>
2023-09-14 12:53:05 -0700

    0x10. C - Variadic functions  - 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - prints strings.
+ * @separator: the separator
+ * @n: size of arguments
+ * Return: Nothing
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list list;
+
+	if (!n)
+	{
+		printf("\n");
+		exit(1);
+	}
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(list, char *);
+
+		printf("%s", str);
+
+		if (i < n - 1)
+		{
+			printf("%s", separator ? separator : "");
+		}
+	}
+	va_end(list);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,8 +2,10 @@
 #define VARIADIC_FUNCTION
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif

516ac0e199a56b9120079544dce4d58f1844a065
samehelgazar95 <selgazar95@gmail.com>
2023-09-22 15:03:40 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * printf_before_main- printe message befoe main
+ * Return: Nothing
+*/
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

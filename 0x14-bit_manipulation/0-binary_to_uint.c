ddc5f16bf5420440ac45a14730ea7295f3b3662f
samehelgazar95 <selgazar95@gmail.com>
2023-09-28 07:17:40 -0700

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: the binary string number
+ * Return: the decimal number
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	int length = _strlen(b), i, j;
+	unsigned int current, total = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = length - 1, j = 0; i >= 0; i--, j++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		if (b[i] == '1')
+		{
+			current = twoPow(j);
+			total += current;
+		}
+	}
+
+	return (total);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+int _strlen(const char *str);
+unsigned int twoPow(int powVal);
+unsigned int binary_to_uint(const char *b);
+
+#endif

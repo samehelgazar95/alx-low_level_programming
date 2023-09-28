23ecfc58354aa37f8c032c2dc0c46572d24501df
samehelgazar95 <selgazar95@gmail.com>
2023-09-28 07:40:35 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,44 @@
 #include "main.h"
 
 /**
+ * _strlen - Calc the str len
+ * @str: the string
+ * Return: the length
+*/
+
+int _strlen(const char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
+
+/**
+ * twoPow - Calc the power val
+ * @powVal: the powered to val
+ * Return: the powered value
+*/
+
+unsigned int twoPow(int powVal)
+{
+	unsigned int sum = 1;
+
+	if (powVal < 0)
+		return (0);
+	
+	while (powVal > 0)
+	{
+		sum *= 2;
+		powVal--;
+	}
+
+	return (sum);
+}
+
+/**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: the binary string number
  * Return: the decimal number
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,8 +3,6 @@
 
 #include <stddef.h>
 
-int _strlen(const char *str);
-unsigned int twoPow(int powVal);
 unsigned int binary_to_uint(const char *b);
 
 #endif

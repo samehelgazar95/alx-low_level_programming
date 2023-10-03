6b4cc4e55153076ad1aadddd4b230e6ac95f022a
samehelgazar95 <selgazar95@gmail.com>
2023-10-03 15:08:57 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -43,8 +43,7 @@ int create_file(const char *filename, char *text_content)
 	if (len)
 		bytes_wrote = write(fd, text_content, len);
 
-	if (close(fd) < 0)
-		return (-1);
+	close(fd);
 
 	return (bytes_wrote ? 1 : -1);
 }

6386aec1ba7d3c339ceab21cb81ab8dcbc4d5828
samehelgazar95 <selgazar95@gmail.com>
2023-10-03 15:05:06 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,8 @@ int stringlen(char *str)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int bytes_wrote = 0, len = stringlen(text_content);
+	ssize_t bytes_wrote = 0;
+	int len = stringlen(text_content);
 
 	if (!filename)
 		return (-1);

9937ae94df5aa5099c368994b15930d2b97f4b32
samehelgazar95 <selgazar95@gmail.com>
2023-10-03 10:07:32 -0700

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t len = 0;
-	const list_t *temp = h;
+	const listint_t *temp = h;
 	list_t *nonConstTemp = (list_t *)temp;
 
 	while (temp != NULL)
---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * read_textfile- reads a text file and prints it
+ * @filename: the file
+ * @letters: the count
+ * Return: number of chars read
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, read_count;
+	char *read_text = calloc(letters, sizeof(char));
+
+	if (read_text == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+
+	read_count = read(fd, read_text, letters);
+	if (read_count < 0)
+		return (0);
+
+	dprintf(STDOUT_FILENO, "%s", read_text);
+
+
+	if (close(fd) < 0)
+		return (0);
+
+	return (read_count);
+}

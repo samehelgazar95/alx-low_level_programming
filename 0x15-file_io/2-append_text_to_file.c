4893663629780251d542894a894727b9b50fbdb0
samehelgazar95 <selgazar95@gmail.com>
2023-10-03 16:38:54 -0700

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,22 +21,6 @@ int _strlen(char *str)
 }
 
 /**
- * perm_check- checking the file permission
- * @filename: the file
- * Return: 1 or -1
-*/
-
-int perm_check(const char *filename)
-{
-	struct stat file_info;
-	
-	if (stat(filename, &file_info) == 0)
-		return ((file_info.st_mode & S_IWGRP) == S_IWGRP);
-	else
-		return (0);
-}
-
-/**
  * append_text_to_file- append text to an existing file
  * @filename: the file
  * @text_content: the text
@@ -49,9 +33,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	ssize_t len = _strlen(text_content), bytes;
 
 	if (!filename)
-		return (-1);
-
-	if (!perm_check(filename))
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);

aa1c746f79d418db218de96145d716782d035ae0
Sameh ElGazar <selgazar95@gmail.com>
2023-12-04 03:33:03 +0200

    Task 4.2

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,7 +16,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	temp = ht->array[index];
-	while (temp)
+	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
 			return (temp->value);
